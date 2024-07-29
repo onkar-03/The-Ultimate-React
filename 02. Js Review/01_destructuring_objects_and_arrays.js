7bf92547abea9a52064d5e52b8d95b529b9e6e2a
Onkar Patel <onkarpatel10@gmail.com>
2024-07-29 23:26:03 +0530

    Revising Destructuting Arrays & Objects necessary for React

---
+++ 02. Js Review/01_destructuring_objects_and_arrays.js
@@ -0,0 +1,182 @@
+// Array that contains 5 Book Objects
+const data = [
+  // Book 1
+  {
+    id: 1,
+    title: 'The Lord of the Rings',
+    publicationDate: '1954-07-29',
+    author: 'J. R. R. Tolkien',
+    genres: [
+      'fantasy',
+      'high-fantasy',
+      'adventure',
+      'fiction',
+      'novels',
+      'literature',
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: 'El señor de los anillos',
+      chinese: '魔戒',
+      french: 'Le Seigneur des anneaux',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  // Book 2
+  {
+    id: 2,
+    title: 'The Cyberiad',
+    publicationDate: '1965-01-01',
+    author: 'Stanislaw Lem',
+    genres: [
+      'science fiction',
+      'humor',
+      'speculative fiction',
+      'short stories',
+      'fantasy',
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  // Book 3
+  {
+    id: 3,
+    title: 'Dune',
+    publicationDate: '1965-01-01',
+    author: 'Frank Herbert',
+    genres: ['science fiction', 'novel', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: '',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  // Book 4
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: '1997-06-26',
+    author: 'J. K. Rowling',
+    genres: ['fantasy', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: 'Harry Potter y la piedra filosofal',
+      korean: '해리 포터와 마법사의 돌',
+      bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
+      portuguese: 'Harry Potter e a Pedra Filosofal',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  // Book 5
+  {
+    id: 5,
+    title: 'A Game of Thrones',
+    publicationDate: '1996-08-01',
+    author: 'George R. R. Martin',
+    genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: '왕좌의 게임',
+      polish: 'Gra o tron',
+      portuguese: 'A Guerra dos Tronos',
+      spanish: 'Juego de tronos',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+// Returns all the Data
+function getBooks() {
+  return data;
+}
+
+// Returns the Data about Book whose id was passed
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+// --- Destructuring Objects {}
+// The names used while Destructuring Objects should be the same as what the property names are
+
+// Getting Data about a Book
+const book = getBook(2);
+
+// Normal way to get specific data out of the Object
+// const title = book.title;
+// const author = book.author;
+// console.log(title,author);
+
+// Destructuring the Book Data received and retrieving title, author, pages, genres & publicationDate from the data
+const { title, author, pages, genres, publicationDate } = book;
+console.log(author, title, genres);
+
+// --- Destructuring Arrays []
+// Arrays are destructured by the order of elements
+// Here we need to keep in mind the Order of Elements
+
+// Normal way to retrieve the data from the Arrays
+// const primaryGenre = genres[0];
+// const secondaryGenre = genres[1];
+// console.log(primaryGenre, secondaryGenre);
+
+// Destructuring the genres array to retrieve the data
+// The destructuring assignment const [primaryGenre, secondaryGenre] = genres; extracts the first and second elements of the genres array and assigns them to the variables primaryGenre and secondaryGenre, respectively
+const [primaryGenre, secondaryGenre] = genres;
+
+console.log(primaryGenre, secondaryGenre);
