0d09e96fa94a6c8f9c693487ebfdd7b998a1b2f5
Onkar Patel <onkarpatel10@gmail.com>
2024-07-31 00:15:43 +0530

    Leanrd about the .reduce() method of Arrays in Js

---
+++ 02. Js Review/10_array_reduce_method.js
@@ -0,0 +1,180 @@
+// Array that contains 5 Book Objects
+const data = [
+  // Book 1
+  {
+    id: 1,
+    title: 'The Lord of the Rings',
+    publicationDate: '1954-07-29',
+    author: 'J. R. R. Tolkien',
+    genres: [
+      'fantasy',
+      'high-fantasy',
+      'adventure',
+      'fiction',
+      'novels',
+      'literature',
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: 'El señor de los anillos',
+      chinese: '魔戒',
+      french: 'Le Seigneur des anneaux',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  // Book 2
+  {
+    id: 2,
+    title: 'The Cyberiad',
+    publicationDate: '1965-01-01',
+    author: 'Stanislaw Lem',
+    genres: [
+      'science fiction',
+      'humor',
+      'speculative fiction',
+      'short stories',
+      'fantasy',
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  // Book 3
+  {
+    id: 3,
+    title: 'Dune',
+    publicationDate: '1965-01-01',
+    author: 'Frank Herbert',
+    genres: ['science fiction', 'novel', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: '',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  // Book 4
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: '1997-06-26',
+    author: 'J. K. Rowling',
+    genres: ['fantasy', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: 'Harry Potter y la piedra filosofal',
+      korean: '해리 포터와 마법사의 돌',
+      bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
+      portuguese: 'Harry Potter e a Pedra Filosofal',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  // Book 5
+  {
+    id: 5,
+    title: 'A Game of Thrones',
+    publicationDate: '1996-08-01',
+    author: 'George R. R. Martin',
+    genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: '왕좌의 게임',
+      polish: 'Gra o tron',
+      portuguese: 'A Guerra dos Tronos',
+      spanish: 'Juego de tronos',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+// Returns all the Data
+function getBooks() {
+  return data;
+}
+
+// Returns the Data about Book whose id was passed
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+// --- Array Methods
+// The Array methods .map(), .filter(), .reduce() are called functional array methods as they do not mutate the original array instead return a new array based on original one
+
+// Getting all books from the Data
+const books = getBooks();
+books;
+
+// 3. Reduce Method
+// It is the most powerful & most versatile method of Arrays in Javascript
+// The .reduce() method in JavaScript is used to execute a reducer function on each element of the array, resulting in a single output value
+// This method is useful for accumulating values, such as summing numbers, creating a cumulative result, or transforming data into a different structure
+// Reduce expects a callback function inside of it for which each of the element will be called and processed, it also takes in a starter value too such as an accumulator
+
+// Syntax: .reduce((accumulator, otherParameter), startValueOfAccumulator)
+
+// Calculating the Total Number of Pages in all Books, acc start value as 0
+const pageAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+console.log(pageAllBooks);
+
+// The initial value can be anything a Number , Array, Object etc.. hence we can also do all of the filter,  map methods and complex calculations too using the reduce method
+// You can dig deep into it if u want on the Web
