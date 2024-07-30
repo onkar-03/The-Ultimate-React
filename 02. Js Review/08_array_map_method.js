44878df24f11e52d9f9ad57435c46ed06c48d8a4
Onkar Patel <onkarpatel10@gmail.com>
2024-07-30 23:52:46 +0530

    Learned about map() method of Arrays in Js

---
+++ 02. Js Review/08_array_map_method.js
@@ -0,0 +1,187 @@
+// Array that contains 5 Book Objects
+const data = [
+  // Book 1
+  {
+    id: 1,
+    title: 'The Lord of the Rings',
+    publicationDate: '1954-07-29',
+    author: 'J. R. R. Tolkien',
+    genres: [
+      'fantasy',
+      'high-fantasy',
+      'adventure',
+      'fiction',
+      'novels',
+      'literature',
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: 'El señor de los anillos',
+      chinese: '魔戒',
+      french: 'Le Seigneur des anneaux',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  // Book 2
+  {
+    id: 2,
+    title: 'The Cyberiad',
+    publicationDate: '1965-01-01',
+    author: 'Stanislaw Lem',
+    genres: [
+      'science fiction',
+      'humor',
+      'speculative fiction',
+      'short stories',
+      'fantasy',
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  // Book 3
+  {
+    id: 3,
+    title: 'Dune',
+    publicationDate: '1965-01-01',
+    author: 'Frank Herbert',
+    genres: ['science fiction', 'novel', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: '',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  // Book 4
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: '1997-06-26',
+    author: 'J. K. Rowling',
+    genres: ['fantasy', 'adventure'],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: 'Harry Potter y la piedra filosofal',
+      korean: '해리 포터와 마법사의 돌',
+      bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
+      portuguese: 'Harry Potter e a Pedra Filosofal',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  // Book 5
+  {
+    id: 5,
+    title: 'A Game of Thrones',
+    publicationDate: '1996-08-01',
+    author: 'George R. R. Martin',
+    genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: '왕좌의 게임',
+      polish: 'Gra o tron',
+      portuguese: 'A Guerra dos Tronos',
+      spanish: 'Juego de tronos',
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+// Returns all the Data
+function getBooks() {
+  return data;
+}
+
+// Returns the Data about Book whose id was passed
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+// --- Array Methods
+// The Array methods .map(), .filter(), .reduce() are called functional array methods as they do not mutate the original array instead return a new array based on original one
+
+// Getting all books from the Data
+const books = getBooks();
+books;
+
+// 1. Map Method:
+// in JavaScript .map() method used to create a new array by applying a provided function to each element of an existing array
+// Map expects a callback function inside of it for which each of the element will be called and processed
+// It does not modify the original array but returns a new one
+
+// Storing the 2X values in 'x' as an Array
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+// Storing the titles of all Books in 'titles' Array
+const titles = books.map((book) => book.title);
+console.log(titles);
+
+// Storing only the essential info like title & author in 'essentialData' Array
+// To return an Object without return in arrow function we need to enclose it in () parenthesis
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewCount: getTotalReviewCount(book),
+}));
+console.log(essentialData);
