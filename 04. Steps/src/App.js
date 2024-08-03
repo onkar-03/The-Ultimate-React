dae9327cd8abaac4b37aba14b04cb68ddeea56e5
Onkar Patel <onkarpatel10@gmail.com>
2024-08-03 23:46:33 +0530

    Building the Overlay of the Steps Component

--- 04. Steps/src/App.js
+++ 04. Steps/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// Root Component 'App'
+// Exporting so as the index.js can use it where it is imported
 
-function App() {
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+export default function App() {
+  // Current Step
+  const step = 1;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className='steps'>
+        <div className='numbers'>
+          <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+          <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+          <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        </div>
+
+        <p className='message'>
+          Step {step}: {messages[step - 1]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <div className='buttons'>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+            Prev
+          </button>
+          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+            Next
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
