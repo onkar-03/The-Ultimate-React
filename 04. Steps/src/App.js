d84fc7fb803e8b7375339883f0339c8f264401f2
Onkar Patel <onkarpatel10@gmail.com>
2024-08-04 02:15:22 +0530

    Learend about State and its Importance in React

--- 04. Steps/src/App.js
+++ 04. Steps/src/App.js
@@ -1,22 +1,47 @@
-// Root Component 'App'
-// Exporting so as the index.js can use it where it is imported
-
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
   'Invest your new income 🤑',
 ];
 
+/*
+ --- Event Listeners in React
+ - In React, we use a more declarative approach so we do not manually select Dom elements and so therefore we do also not use add event listener
+ - We do something very similar to the HTML inline event listeners. So basically we will directly listen for the event right on the element where they will happen.
+ - In React, event listeners are written as attributes in JSX
+ - These attributes have a prefix 'on' and are camel-cased unlike HTML, and their values are callback functions that will be called when the event occurs inside {} as we need to write in Js mode inside JSX
+ - We generally write these event handlers Functions separately inside the JSX and then call them in the Event Handlers
+*/
+
+/* 
+ --- State in React
+ - State is the most important concept in React
+ - State refers to the general condition in which a component is
+ - We have already learned how to pass data to a component using props
+ - State is components own data that the component holds overtime, necessary for information that it needs to hold throughout the app's cycle
+ - State: Components Memory
+ - Piece of State: a State variable in a component
+ - Updating component state triggers React to re-render the component
+*/
+
+/*
+ --- What does state do ??
+ - Updates the Components View
+ - Persist local variables between renders & re-renders
+ */
+
+// Root Component 'App'
+// Exporting so as the index.js can use it where it is imported
 export default function App() {
   // Current Step
-  const step = 1;
+  let step = 1;
 
   // Event Handler Functions
   function handlePrevious() {
-    alert('Previous');
+    step--;
   }
   function handleNext() {
-    alert('Next');
+    step++;
   }
 
   return (
@@ -35,8 +60,9 @@ export default function App() {
           Step {step}: {messages[step - 1]}
         </p>
 
-        {/* Used inline CSS for buttons */}
         <div className='buttons'>
+          {/* Used inline CSS for buttons */}
+          {/* Adding Event Listeners in JSX */}
           <button
             style={{ backgroundColor: '#7950f2', color: '#fff' }}
             onClick={handlePrevious}
