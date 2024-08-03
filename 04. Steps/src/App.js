89bda37a31e3527be9717dc9e57e25b9be676854
Onkar Patel <onkarpatel10@gmail.com>
2024-08-03 23:59:04 +0530

    Handling Events in React Components

--- 04. Steps/src/App.js
+++ 04. Steps/src/App.js
@@ -11,24 +11,42 @@ export default function App() {
   // Current Step
   const step = 1;
 
+  // Event Handler Functions
+  function handlePrevious() {
+    alert('Previous');
+  }
+  function handleNext() {
+    alert('Next');
+  }
+
   return (
     <>
       <div className='steps'>
         <div className='numbers'>
+          {/* Conditional rendering of active class based on current step */}
           <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
           <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
           <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
         </div>
 
         <p className='message'>
+          {/* Displaying the step & message of the current Step */}
+          {/* As the Array indexing starts from 0 hence we did step - 1 */}
           Step {step}: {messages[step - 1]}
         </p>
 
+        {/* Used inline CSS for buttons */}
         <div className='buttons'>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          <button
+            style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            onClick={handlePrevious}
+          >
             Prev
           </button>
-          <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          <button
+            style={{ backgroundColor: '#7950f2', color: '#fff' }}
+            onClick={handleNext}
+          >
             Next
           </button>
         </div>
