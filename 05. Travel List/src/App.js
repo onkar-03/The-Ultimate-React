b0c7bcbebd06ecb0bed065c125c9b60afe36766f
Onkar Patel <onkarpatel10@gmail.com>
2024-08-08 01:04:09 +0530

    Implementing Controlled Elements in React

--- 05. Travel List/src/App.js
+++ 05. Travel List/src/App.js
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: true },
 ];
 
+/* 
+ --- Controlled Elements In React ---
+
+ --- Default Behavior of Input Fields
+
+ - By default, input fields such as `<input>` and `<select>` elements maintain their own state within the DOM. This makes it challenging to read their values and manage their state effectively.
+ - In React, we prefer to keep all state in a single, central place within the React application, rather than in the DOM.
+
+ --- Controlled Elements
+
+ - To achieve centralized state management, we use a technique called controlled elements. This means that React controls and owns the state of these input fields, not the DOM
+ - Doing this helps us achieve the fact that now React controls teh States of input / select fields of the Form element
+
+ --- Steps to Implement Controlled Elements
+ 1. Create a Piece of State: 
+   - EG: const [description, setDescription] = useState('');
+
+ 2. Use State as Value of Input Field: 
+ - Force the element to always take the Value of the State variable 
+  - EG: <input type="text" value={description} />
+
+ 3. Listen for Change Event
+  - EG: <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+
+*/
 export default function App() {
   return (
     <div className='app'>
@@ -19,10 +46,25 @@ function Logo() {
 }
 
 function Form() {
-  // Event handlers
+  // Implementing Controlled Elements for Select and Form elements
+  // 1. Creating a Piece of State, its default value is an empty string
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  // Event handlers Functions
   function handleSubmit(e) {
     // To disable the reloading of page on items addition
     e.preventDefault();
+
+    // If no Item description then do nothing
+    if (!description) return;
+
+    // Creating the New Item Object
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // Rest all the Values back to Default using setter functions
+    setDescription('');
+    setQuantity(1);
   }
 
   return (
@@ -32,7 +74,22 @@ function Form() {
     // The handleSubmit takes the current Event and uses it in teh event handler
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 🥳 trip ??</h3>
-      <select name='quantity'>
+
+      {/*
+      - Controlled Elements Step 2 
+      - 2. Using the piece of State as value of input field for Controlled Elements
+      - 3. Listening for Change Event for Controlled Elements in teh input field using onChange() EventListener
+      */}
+      <select
+        name='quantity'
+        value={quantity}
+        // Whenever we type in the Input field a change event is fired up
+        // We handle the change using the onChange() EventHandler
+        // The function receives the event and on the event we read the target that is the input element
+        // e.target.value: is the text that we typed in the input field
+        // e.target.value is always a string, to place it in teh state first convert it to a number using Number() / +
+        onChange={(e) => setQuantity(+e.target.value)}
+      >
         {/* 
         - Creating 20 options using Array.from() and .map()
         - Each number is rendered as an option value inside the select element
@@ -44,7 +101,22 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type='text' placeholder='Item...' />
+
+      {/*
+      - Controlled Elements Step 2 
+      - 2. Using the piece of State as value of input field for Controlled Elements
+      - 3. Listening for Change Event for Controlled Elements in teh input field using onChange() EventListener
+      */}
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        // Whenever we type in the Input field a change event is fired up
+        // We handle the change using the onChange() EventHandler
+        // The function receives the event and on the event we read the target that is the input element
+        // e.target.value: is the text that we typed in the input field
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button type='submit'>Add</button>
     </form>
   );
@@ -79,7 +151,7 @@ function Item({ item }) {
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick=''>❌</button>
+      <button>❌</button>
     </li>
   );
 }
