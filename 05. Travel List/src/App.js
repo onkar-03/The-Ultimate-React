a79d91a5aa31c3cf67ebfa8f58b1f9a62017e396
Onkar Patel <onkarpatel10@gmail.com>
2024-08-11 03:17:52 +0530

    Deleting Items from the Items list using child to parent communication

--- 05. Travel List/src/App.js
+++ 05. Travel List/src/App.js
@@ -8,8 +8,8 @@ const initialItems = [
 // App Component
 export default function App() {
   // 1. Creating a Piece of State to handle the Items list
-  // Default items list must be empty at the start hence we use an y array
-  const [items, setItems] = useState([]);
+  // Default items list must be empty at the start hence we use an empty array [], or we can pass te default values too if we want i.e. the initialItems
+  const [items, setItems] = useState(initialItems);
 
   //Event Handler for adding Items to the List
   function handleAddItems(item) {
@@ -19,14 +19,26 @@ export default function App() {
     setItems((items) => [...items, item]);
   }
 
+  // Function to delete Items from the list
+  // To know which item to delete we must know the id of the item in the list
+  function handleDeleteItems(id) {
+    // --- Updating Items without Mutating the Original Array
+    // We can't mutate the Original value in React
+    // As we dont want to mutate the original array, hence we create a new array and then destructure the elements of the old array and add the items that are not equal to the item to be deleted along with the rest of them
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
   return (
     <div className='app'>
       <Logo />
 
       {/* As we require the handlerItems handler in the Form Component hence we pass it as a prop in the Form component */}
       <Form onAddItems={handleAddItems} />
-      {/* As we require the items in the PackingList Component hence we pass it as a prop in the PackingList Component */}
-      <PackingList items={items} />
+      {/*
+       - As we require the items in the PackingList Component hence we pass it as a prop in the PackingList Component 
+       - Passing the delete items as a prop in the PackingList Component to delete the item when we click the cross icon
+      */}
+      <PackingList items={items} onDeleteItem={handleDeleteItems} />
       <Stats />
     </div>
   );
@@ -68,14 +80,14 @@ function Form({ onAddItems }) {
     // Rendering Form and its components
     // Listening for Submit event on the Form
     // The submit event happens when we click the add button / press Enter on keyboard
-    // The handleSubmit takes the current Event and uses it in teh event handler
+    // The handleSubmit takes the current Event and uses it in the event handler
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 🥳 trip ??</h3>
 
       {/*
       - Controlled Elements Step 2 
       - 2. Using the piece of State as value of input field for Controlled Elements
-      - 3. Listening for Change Event for Controlled Elements in teh input field using onChange() EventListener
+      - 3. Listening for Change Event for Controlled Elements in the input field using onChange() EventListener
       */}
       <select
         name='quantity'
@@ -84,7 +96,7 @@ function Form({ onAddItems }) {
         // We handle the change using the onChange() EventHandler
         // The function receives the event and on the event we read the target that is the input element
         // e.target.value: is the text that we typed in the input field
-        // e.target.value is always a string, to place it in teh state first convert it to a number using Number() / +
+        // e.target.value is always a string, to place it in the state first convert it to a number using Number() / +
         onChange={(e) => setQuantity(+e.target.value)}
       >
         {/* 
@@ -102,7 +114,7 @@ function Form({ onAddItems }) {
       {/*
       - Controlled Elements Step 2 
       - 2. Using the piece of State as value of input field for Controlled Elements
-      - 3. Listening for Change Event for Controlled Elements in teh input field using onChange() EventListener
+      - 3. Listening for Change Event for Controlled Elements in the input field using onChange() EventListener
       */}
       <input
         type='text'
@@ -119,8 +131,8 @@ function Form({ onAddItems }) {
   );
 }
 
-// Destructuring the items prop that contains the list of items that needs to be rendered in the UI
-function PackingList({ items }) {
+// Destructuring the items prop that contains the list of items that needs to be rendered in the UI & onDeleteItem method that has the id of the item to be deleted
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className='list'>
       <ul>
@@ -128,10 +140,11 @@ function PackingList({ items }) {
       - Rendering Items List using Map method for the whole items array 
       - Each item is passed as a prop to Item component
       - Destructured in the item component and used directly 
-      - Whenever we map over a list to render it we need to give a key value to each value of the list to identify them as unique items
+      - Whenever we map over a list to render it we need to give a key value to each value of the list to identify them as unique items\
+      - Using State lifting method to pass the onDeleteItem method to the child component i.e. the Item
       */}
         {items.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
@@ -139,7 +152,8 @@ function PackingList({ items }) {
 }
 
 // Destructuring the Item received as Props then we can use the Oject directly
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
+  // The onDeleteItem passed from APP -> PackingList -> Item to be used here using State Lifting Technique
   return (
     <li>
       {/* 
@@ -149,7 +163,13 @@ function Item({ item }) {
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      {/*
+      --- Child to Parent Communication (Child Updates Parent State)
+      -  We need to call the event with the item id only and not the Object hence
+      we pass in the item.id 
+      - Also as we want the react to call the function when event happens we can not directly call te function as onDeleteItem(item.id) rather as a callback function like () => onDeleteItem(item.id)
+      */}
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
