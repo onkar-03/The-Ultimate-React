dc9f71b613f93c1b6bdb06436dff2aaa48528318
Onkar Patel <onkarpatel10@gmail.com>
2024-09-11 23:59:46 +0530

    Implementing useKey Custom hook for eventhandlers

---
+++ 07. PopFlix/src/useKey.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+// This Custom Hook handles attaching and removing Event Handlers on keypress
+export function useKey(key, action) {
+  // Side Effect to enable Esc keystroke for going back
+  useEffect(
+    function () {
+      function callback(e) {
+        // Call CloseMovie only if the key is Escape
+        // We convert the strings to lower case just o avoid capitalization errors
+        if (e.code.toLowerCase() === key.toLowerCase()) {
+          action();
+        }
+      }
+
+      // Add the Event Listener on the Document for keypress
+      document.addEventListener('keydown', callback);
+
+      // Cleanup Function
+      return () => document.removeEventListener('keydown', callback);
+    },
+
+    // Re render if action / key changes as arguments for Custom Hook
+    [action, key],
+  );
+}
