9c821bcc8e14b2fd3276ae3a0748914bd2e3b9bf
Onkar Patel <onkarpatel10@gmail.com>
2024-09-14 23:12:34 +0530

    Managing related pieces of State

--- 10. React Quiz/Theory.md
+++ 10. React Quiz/Theory.md
@@ -2,17 +2,15 @@
 
 ## Initializing the `useReducer` Hook
 
-In this step, we introduce the `useReducer` hook. This hook is an alternative to `useState`, especially useful when state logic becomes complex. The `useReducer` hook requires two key components:
+In this step, we introduce the `useReducer` hook. This hook is an alternative to `useState`, especially useful when state logic becomes complex.We generally use `useReducer` when we have some complex state to manage.
 
-- A `reducer` function that determines the next state.
-- An initial state.
+The complex state means it generally is an object and not a single value while using `useReducer`
 
-The `useReducer` function returns an array containing two elements:
+The `useReducer` hook requires two key components:
 
-- The current state.
-- A `dispatch` function to trigger state updates.
+- A `reducer` function that determines the next state. It has access to the current state and the action provided to it from teh dispatch
 
-The `dispatch` function allows us to send an action to the reducer, which will compute the new state based on the current state and the action received.
+- The `dispatch` function allows us to send an action to the reducer, which will compute the new state based on the current state and the action received. It is a function that trigger state updates.
 
 ```jsx
 import { useReducer } from 'react';
@@ -41,7 +39,8 @@ The reducer function is essential in managing state transitions. It takes two ar
 1. **`state`**: The current state of the component.
 2. **`action`**: The action that was dispatched to change the state.
 
-The reducer function should always return a new state based on the action. It follows a pure function paradigm, meaning it must not have side effects and should produce the same output given the same input.
+- The reducer function should always return a new state based on the action.
+- It follows a pure function paradigm, meaning it must not have side effects and should produce the same output given the same input.
 
 ```jsx
 function reducer(state, action) {
@@ -51,9 +50,11 @@ function reducer(state, action) {
 
 ## Using `dispatch` to Update State
 
-The `dispatch` function is used to send actions to the reducer, causing it to recalculate the state. You can think of it as a trigger for a state change. Each time an action is dispatched, the reducer function is called with the current state and the action, which then returns the updated state.
+- The `dispatch` function is used to send actions to the reducer, causing it to recalculate the state. You can think of it as a trigger for a state change.
 
-By using the `dispatch` function, you can separate the logic for handling state transitions from the actual UI, making your code more predictable and easier to debug.
+- Each time an action is dispatched, the reducer function is called with the current state and the action, which then returns the updated state.
+
+- By using the `dispatch` function, you can separate the logic for handling state transitions from the actual UI, making your code more predictable and easier to debug.
 
 ```jsx
 <button onClick={() => dispatch(1)}>Increment</button>
--- 10. React Quiz/src/DateCounter.js
+++ 10. React Quiz/src/DateCounter.js
@@ -1,32 +1,59 @@
 import { useReducer, useState } from 'react';
 
+// Complex Initial State (an Object)
+const initialState = { count: 0, step: 1 };
+
 // --- Reducer Function of the useReducer Hook
 // It gets access to the current state and the action that we pass in the dispatch function
 // Takes in all the details about the action and the current state
 // Based on action type takes in some kind of actions and returns the desired results
 function reducer(state, action) {
   console.log(state, action);
+
   // return state + action;
 
-  if (action.type === 'inc') return state + 1;
-  if (action.type === 'dec') return state - 1;
-  if (action.type === 'setCount') return action.payload;
+  // if (action.type === 'inc') return state + 1;
+  // if (action.type === 'dec') return state - 1;
+  // if (action.type === 'setCount') return action.payload;
+
+  // For different action types inputs we generally use the Switch Statement
+  switch (action.type) {
+    case 'dec':
+      // As state is an Object, hence we destructure it first and then update the count value according to the action
+      // Access property of State Object 'count' as state.count, 'step' as state.step etc...
+      return { ...state, count: state.count - state.step };
+    case 'inc':
+      return { ...state, count: state.count + state.step };
+    case 'setCount':
+      return { ...state, count: +action.payload };
+    case 'setStep':
+      return { ...state, step: +action.payload };
+    case 'reset':
+      // For reset we want the initial state values
+      return initialState;
+    default:
+      throw new Error('Invalid action');
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
+  // const [step, setStep] = useState(1);
 
   // --- Reducer Hook
-  // Takes the initial state and also the reducer function
+  // Takes the initial state and also the reducer function, the initial state while using useReducer is generally a Complex State
   // Returns the updated state and the dispatch function
 
   // --- Dispatch Function of the useReducer Hook
   // The dispatch function is similar to the setter function of the useState Hook,
   // But in the Dispatch Function we pass in actions that we want to carry out in the reducer function
   // The dispatch Actions are primarily an Object with properties like 'type:' & optionally 'payload:' which contains the value that we want to pass in to the reducer function
-  const [count, dispatch] = useReducer(reducer, 0);
 
-  const [step, setStep] = useState(1);
+  // Passing Complex State to useReducer Hook
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Destructuring the Complex State returned from the useReducer Hook
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
@@ -47,17 +74,22 @@ function DateCounter() {
   };
 
   const defineCount = function (e) {
-    dispatch({ type: 'setCount', payload: +e.target.value });
     // setCount(Number(e.target.value));
+
+    dispatch({ type: 'setCount', payload: +e.target.value });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+
+    dispatch({ type: 'setStep', payload: +e.target.value });
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
+
+    dispatch({ type: 'reset' });
   };
 
   return (
