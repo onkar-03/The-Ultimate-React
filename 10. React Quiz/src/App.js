ad9fe8554e11953b5241132f1f26c7f458563def
Onkar Patel <onkarpatel10@gmail.com>
2024-09-15 23:30:31 +0530

    Creating the React Quiz App

--- 10. React Quiz/src/App.js
+++ 10. React Quiz/src/App.js
@@ -1,9 +1,15 @@
-import DateCounter from './DateCounter.js';
+import Header from './Header.js';
+import Main from './Main.js';
+// import DateCounter from './DateCounter.js';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className='app'>
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
