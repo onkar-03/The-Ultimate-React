2d3d8a5ac45798b314c73be5b3bfb429b386ef7c
Onkar Patel <onkarpatel10@gmail.com>
2024-09-26 00:20:47 +0530

    Build the Apps Static Layout

--- 11. WorldWise/src/components/Logo.jsx
+++ 11. WorldWise/src/components/Logo.jsx
@@ -1,13 +1,7 @@
 import styles from './Logo.module.css';
-import { Link } from 'react-router-dom';
 
 function Logo() {
-  return (
-    // Creating the Image as a Link to open home page whenever we click on the Image
-    <Link to='/'>
-      <img src='/logo.png' alt='WorldWise logo' className={styles.logo} />
-    </Link>
-  );
+  return <img src='/logo.png' alt='WorldWise logo' className={styles.logo} />;
 }
 
 export default Logo;
