c5685a5d1da05a9579587bb6d01b474ff4a67c62
Onkar Patel <onkarpatel10@gmail.com>
2024-11-03 20:35:18 +0530

    Added the City Markers

--- 11. WorldWise/src/components/Map.jsx
+++ 11. WorldWise/src/components/Map.jsx
@@ -2,6 +2,7 @@ import styles from './Map.module.css';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 function Map() {
   // Lat and Lng passed to URL in the CityItem Component
@@ -15,6 +16,10 @@ function Map() {
   // Now this navigate function is used to move to any URL that we want
   // We define the Path inside the navigate("Path")
   const navigate = useNavigate();
+
+  // Cities Marker
+  // We need to get access to the cities data here wis were our CitiesContext that we created is used
+  const { cities } = useCities();
 
   // We cant directly access the params we need to use the .get() function
   const lat = searchParams.get('lat');
@@ -32,11 +37,21 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+
+        {/* 
+        - Cities is an Array hence we loop over it to render a City Marker for every City 
+         */}
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
